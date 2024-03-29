1344f5d4386fe943da2be3912366d81bd2f3c12b
Mercy Wanjiru <wanjirumercy54@gmail.com>
2024-03-29 18:43:31 +0300

    Completes lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+
+const returnFirstTwoDrivers = function (array) {
+  return array.slice(0, 2);
+};
+console.log(returnFirstTwoDrivers(drivers));
+
+const returnLastTwoDrivers = function (array) {
+  return array.slice(-2);
+};
+
+// console.log(returnLastTwoDrivers(drivers));
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+// console.log(selectingDrivers.slice(0, 1));
+
+function createFareMultiplier(number) {
+  return function multiply(fare) {
+    return fare * number;
+  };
+}
+console.log(createFareMultiplier(1)(4));
+
+const fareDoubler = createFareMultiplier(2);
+console.log(fareDoubler(10));
+
+const fareTripler = createFareMultiplier(3);
+console.log(fareTripler(10));
+
+function selectDifferentDrivers(x, y) {
+  return y(x);
+}
+
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers));
